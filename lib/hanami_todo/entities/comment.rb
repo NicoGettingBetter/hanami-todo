96c17b83240abf49e2cbab36e47ac3b7f3389682
Mykola Saienko <nicolas.ozz.com@gmail.com>
2020-07-24 00:38:27 +0300

    Add item getter

--- lib/hanami_todo/entities/comment.rb
+++ lib/hanami_todo/entities/comment.rb
@@ -10,4 +10,12 @@ class Comment < ApplicationEntity
   def task_id
     CommentRepository.new.find_with_tasks_comment(id).tasks_comment&.task_id
   end
+
+  def item
+    if schedule_id
+      ScheduleRepository.new.find(schedule_id)
+    else
+      TaskRepository.new.find(task_id)
+    end
+  end
 end
